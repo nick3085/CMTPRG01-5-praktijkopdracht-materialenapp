9cb315a8b2c5509d5b95ed2c662961d1cffe45d3
Nick de Ronde <nick3085@gmail.com>
2017-09-09 21:54:53 +0200

    Added materialmanager app

--- materialenapp/materialmanager/views.py
+++ materialenapp/materialmanager/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. this is also the controller. and the templates are the views.
+# The view on Django tends to be a set of queries for fetching data, and pass them to the template.
